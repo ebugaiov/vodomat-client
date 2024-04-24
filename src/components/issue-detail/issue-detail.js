18ba70c61cd7ce5311aa32a91eaddfa5f59e19bd
EvgeniyBugaiov <evgeniy.bugaiov@gmail.com>
2024-04-24 20:06:22 +0200

    add icon external link in issue

--- src/components/issue-detail/issue-detail.js
+++ src/components/issue-detail/issue-detail.js
@@ -73,9 +73,9 @@ export default class IssueDetail extends Component {
                 <h6 className="card-title">{ address }</h6>
                 <p className="card-text text-muted">
                     <i className="card-icon fas fa-shopping-cart"></i>&nbsp;
-                    <a target='_blank' rel='noopener noreferrer'
+                    <a target='_blank' rel='noopener noreferrer' className='text-info'
                        href={`/order?avtomat_number=${avtomatNumber}&date=${createdAt.split(' ')[0]}`}>
-                        { avtomatNumber }
+                        {avtomatNumber}&nbsp;<i className="fas fa-external-link-alt fa-xs"></i>
                     </a>
                 </p>
                 <p className="card-text text-muted">
